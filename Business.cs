90dbc502ca8dc7559f69728270e68b91f4358629
stanzip <37351575+stanzip@users.noreply.github.com>
2020-02-25 08:20:16 -0600

    Add files via upload

---
+++ Business.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assign3
+{
+    public class Business : Property
+
+    {
+        string name;
+        readonly enum BusinessType { Grocery, Bank, Repair, FastFood, DepartmentStore }
+        readonly string yearEstablished;
+        uint activeRecruitment;
+
+        public Business() : base()
+            {
+            name = "";
+            yearEstablished = "";
+            activeRecruitment = 0;
+            }
+
+        public Business(string[] input) : base(input)
+        {
+            name = stringParser();
+            yearEstablished = stringParser();
+            activeRecruitment = uintParser();
+        }
+
+        public override ToString()
+        {
+            return $"{name}, a {BusinessType} type of business, established in {yearEstablished}";
+        }
+
+}
+}
